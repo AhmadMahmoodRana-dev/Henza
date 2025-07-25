776595a088cca47247042e71fa48041adeda47e5
mobin-ahmad <techexpert10885@gmail.com>
2025-07-25 16:10:31 +0500

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,18 +41,11 @@ const Home = () => {
       <div className="w-full min-h-screen xl:px-16 lg:px-12 md:px-10 sm:px-8 px-4 ">
         <HomePageMainCarousel />
         <HomePageCategorySlider heading={"COLLECTIONS"} />
-        {/* <HomePageGridStructure heading={"Shop Luxury Picks"} /> */}
         {Object.entries(groupedProducts).map(([collection, products]) => (
           <div key={collection}>
             <HomePageGridStructure heading={collection} data={products} />
           </div>
         ))}
-        {/* <BrandCardGrid /> */}
-        {/* <LargeCardSlider heading={"Buy Menswear"} products={products} /> */}
-        {/* <LargeCardSlider heading={"Shop Kidswear"} products={products} /> */}
-        {/* <HomePageCategorySlider /> */}
-        {/* <LargeCardSlider heading={"Shop Accessories"} products={products} /> */}
-        {/* <HomePageGridStructure heading={"SUMMER LAWN '25"} /> */}
         <TrendingProducts product={allProductData} />
         {/* <OneTimeShowModel /> */}
       </div>
