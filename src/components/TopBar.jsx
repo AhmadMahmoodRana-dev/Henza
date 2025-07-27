1bd5f05f51e92c96dc4a701a21388c5fe31956ad
mobin-ahmad <techexpert10885@gmail.com>
2025-07-27 14:09:40 +0500

    update Label

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -2,7 +2,10 @@ const TopBar = () => {
   return (
     <div className="bg-[#fc2743] text-white text-xs sm:text-lg tracking-wider  px-2 text-center fixed top-0 w-full z-50 whitespace-nowrap overflow-x-auto">
       <marquee behavior="scroll" direction="left">
-        🚚 Free Shipping Nationwide! 15 Days Return & Exchange Policy 
+        🚚 Free Shipping Nationwide! 15 Days Return & Exchange Policy  <span className="ml-12">No Return or Exchange On Sale Items </span>
+      </marquee>
+      <marquee behavior="scroll" direction="left">
+        
       </marquee>
     </div>
   );
