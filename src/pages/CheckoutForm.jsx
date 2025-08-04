3cd428d2bf8c5c3ae13085bf538250f80fe1e051
mobin-ahmad <techexpert10885@gmail.com>
2025-08-04 18:23:00 +0500

    update

--- src/pages/CheckoutForm.jsx
+++ src/pages/CheckoutForm.jsx
@@ -488,4 +488,4 @@ function OrderSummary({ cartItems }) {
 }
 
 const inputStyle =
-  "w-full px-4 py-1 border-2 border-gray-200 rounded-xl focus:ring-2 focus:ring-[#70c1f3c2] focus:border-[#70c1f3c2] outline-none text-sm transition-all duration-200 placeholder-gray-400 hover:border-gray-300";
+  "w-full px-4 py-3 border-2 border-gray-200 rounded-xl focus:ring-2 focus:ring-[#70c1f3c2] focus:border-[#70c1f3c2] outline-none text-sm transition-all duration-200 placeholder-gray-400 hover:border-gray-300";
